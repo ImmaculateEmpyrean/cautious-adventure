6c5bf42e4dabd135dd965d130a27537e85bdf6a9
Veeru <immaculateempyrean@gmail.com>
2021-07-15 17:24:44 -0700

    started to get the project setup using webpack.

---
+++ webpack.common.js
@@ -0,0 +1,14 @@
+module.exports = {
+	entry: {
+		index: "./src/www/index.js",
+		vendor: "./src/www/vendor.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html/,
+				use: ["html-loader"],
+			},
+		],
+	},
+};
---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+	mode: "development",
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "build"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "./src/www/html/index.html",
+			chunks: ["index"],
+		}),
+		new HtmlWebpackPlugin({
+			filename: "table-display.html",
+			template: "./src/www/html/table-display.html",
+			chunks: [],
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.scss/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+});
