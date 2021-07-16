b4e14ede444d96e02b9598618e501a9740bde8c7
Veeru <immaculateempyrean@gmail.com>
2021-07-16 07:58:53 -0700

    bulma calandar npm plugin added and configured properly.

--- src/www/vendor.js
+++ src/www/vendor.js
@@ -1,1 +1,15 @@
 import "./sass/bulma.scss";
+import bulmaCalendar from 'bulma-calendar/dist/js/bulma-calendar.min.js';
+
+// initialize the bulma calandar npm plugin I doewnloaded from the internet
+document.addEventListener('DOMContentLoaded',function(){
+    // Initialize all input of date type.
+    const calendars = bulmaCalendar.attach('[type="date"]',{
+        startDate: "16/06/2021",
+        min: "16/06/2021", // should be minDate
+        max: "10/10/2024", // should be maxDate
+        showFooter: false,
+        displayMode: "dialog",
+        dateFormat: "dd/MM/yyyy"
+    });
+});
