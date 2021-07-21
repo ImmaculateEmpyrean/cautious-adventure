9fbfa7e1d91566e525db2ca932933bf6f383c8eb
Veeru <immaculateempyrean@gmail.com>
2021-07-21 10:52:32 -0700

    automation now installs npm production packages automatically.

--- deploy-automaton.bat
+++ deploy-automaton.bat
@@ -3,3 +3,6 @@ ssh pi@192.168.1.120 mkdir -p budgetApp/www
 
 scp build/* pi@192.168.1.120:~/budgetApp/www
 scp src/server.js pi@192.168.1.120:~/budgetApp
+scp package.json pi@192.168.1.120:~/budgetApp
+
+ssh -t pi@192.168.1.120 "cd budgetApp; npm install --only=prod"
