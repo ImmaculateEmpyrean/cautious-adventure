51e8e22292a6bbec7ff401a1e31bbc731c815c9a
Veeru <immaculateempyrean@gmail.com>
2021-07-21 10:29:42 -0700

    built automation script to successfully copy files to the linux server.

--- deploy-automaton.bat
+++ deploy-automaton.bat
@@ -1,16 +1,5 @@
-set wwwDestination=pi@web-server:~/budgetApp/www
-set backEndDestination=pi@web-server:~/budgetApp
+ssh pi@192.168.1.120 rm -rf budgetApp
+ssh pi@192.168.1.120 mkdir -p budgetApp/www
 
-echo "deploying to the server specified in the deploy-automaton.bat file"
-echo "wwwDestination : " %wwwDestination%
-echo "backEndDestination : " %backEndDestination%
-
-ssh pi@web-server
-rm -rf budgetApp
-mkdir budgetApp
-cd budgetApp
-mkdir www
-exit
-
-scp build/* %wwwDestination%
-scp src/server.js %backEndDestination%
+scp build/* pi@192.168.1.120:~/budgetApp/www
+scp src/server.js pi@192.168.1.120:~/budgetApp
