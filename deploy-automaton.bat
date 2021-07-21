a260fc9042430cad26e024f40c80aa39339e144d
Veeru <immaculateempyrean@gmail.com>
2021-07-21 12:52:35 -0700

    automation incharge of deployment was finished. it as of the writing stopsthe server if its running and updates its files.

--- deploy-automaton.bat
+++ deploy-automaton.bat
@@ -1,8 +1,10 @@
+ssh pi@192.168.1.120 fuser -k 5000/tcp
 ssh pi@192.168.1.120 rm -rf budgetApp
 ssh pi@192.168.1.120 mkdir -p budgetApp/www
 
 scp build/* pi@192.168.1.120:~/budgetApp/www
 scp src/server.js pi@192.168.1.120:~/budgetApp
 scp package.json pi@192.168.1.120:~/budgetApp
+scp deploy-restart-server-automaton.sh pi@192.168.1.120:~/budgetApp
 
-ssh -t pi@192.168.1.120 "cd budgetApp; npm install --only=prod"
+ssh -t pi@192.168.1.120 "cd budgetApp; npm install --only=prod; chmod +x deploy-restart-server-automaton.sh;"
